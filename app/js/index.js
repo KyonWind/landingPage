3e0314790e9abb41f351587a43aff563e90fc69e
MZZXL <victor.cequeamijares.ext@bayer.com>
2021-03-05 09:08:03 -0300

    header menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,33 @@
-window.onload = function(e) {console.log('Hello from js')} 
+window.onload = function(e) {
+    console.log('Hello from js')
+const btnHamburger = document.querySelector('#hamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const hasfade = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
+
+header.addEventListener('click', function() {
+    console.log("open hamburger");
+    if(header.classList.contains('open'))
+    {
+        console.log("close hamburger");
+        body.classList.remove('noscroll')
+        header.classList.remove('open');
+        hasfade.forEach(function(element){
+            element.classList.add('fade-out')
+        element.classList.remove('fade-in')
+        })
+        
+        
+    } else {
+        hasfade.forEach(function(element){
+            element.classList.add('fade-in')
+            element.classList.remove('fade-out')
+        });
+        body.classList.add('noscroll')
+        header.classList.add('open');
+        
+    }
+    
+});
+} 
